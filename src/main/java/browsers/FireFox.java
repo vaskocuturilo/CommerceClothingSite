ebcd2ad3e139088376240547847eb01ed9d8fa97
asm@dostavista.ru <wolfstonik>
2021-04-28 15:56:33 +0300

    QA-00014: Added FireFox browser.

---
+++ src/main/java/browsers/FireFox.java
@@ -0,0 +1,31 @@
+package browsers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class FireFox implements WebDriverProvider {
+    @Override
+    public WebDriver createDriver(DesiredCapabilities capabilities) {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver(getFirefoxOptions().merge(capabilities));
+    }
+
+    public static FirefoxOptions getFirefoxOptions() {
+        final FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--disable-web-security");
+        firefoxOptions.addArguments("--allow-running-insecure-content");
+
+        final FirefoxProfile profile = new FirefoxProfile();
+        profile.setAcceptUntrustedCertificates(true);
+        profile.setAssumeUntrustedCertificateIssuer(false);
+        profile.setPreference("pageLoadStrategy", "normal");
+
+        firefoxOptions.setCapability(FirefoxDriver.PROFILE, profile);
+
+        return firefoxOptions;
+    }
+}
